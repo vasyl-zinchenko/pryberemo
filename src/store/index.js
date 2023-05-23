fb4b3f5c41e5fa02957383f1157882c48e511081
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2023-05-23 17:03:33 +0300

    fixing images

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,16 @@
 import { defineStore } from "pinia";
 import { computed, reactive, ref, watch } from "vue";
 import feature1 from "../assets/images/features/feature-1.png";
+import feature2_1 from "../assets/images/features/feature-2_1.png";
+import feature2_2 from "../assets/images/features/feature-2_2.png";
+import feature2_3 from "../assets/images/features/feature-2_3.png";
+import feature3 from "../assets/images/features/feature-3.png";
+import feature4 from "../assets/images/features/feature-4.png";
+import kitchenImg from "../assets/images/footer/kitchen.png";
+import bathImg from "../assets/images/footer/bathroom.png";
 
 export const useGeneralStore = defineStore("generalStore", () => {
-	const isMenuVisible = ref(false);
+  const isMenuVisible = ref(false);
   const featureItems = reactive([
     {
       id: 1,
@@ -14,23 +21,19 @@ export const useGeneralStore = defineStore("generalStore", () => {
     {
       id: 2,
       text: "Якісна еко-хімія",
-      img: [
-        "../assets/images/features/feature-2_1.png",
-        "../assets/images/features/feature-2_2.png",
-        "../assets/images/features/feature-2_3.png",
-      ],
+      img: [feature2_1, feature2_2, feature2_3],
       transform: "translateY(30px)",
     },
     {
       id: 3,
       text: "Контролюємо якість",
-      img: ["../assets/images/features/feature-3.png"],
+      img: [feature3],
       transform: "translateY(60px)",
     },
     {
       id: 4,
       text: "Працюємо цілодобово",
-      img: ["../assets/images/features/feature-4.png"],
+      img: [feature4],
       transform: "translateY(90px)",
     },
   ]);
@@ -40,7 +43,7 @@ export const useGeneralStore = defineStore("generalStore", () => {
       id: 1,
       title: "Кухня",
       tab: "CleaningKitchen",
-      image: "src/assets/images/footer/kitchen.png",
+      image: kitchenImg,
     },
     {
       id: 2,
@@ -51,7 +54,7 @@ export const useGeneralStore = defineStore("generalStore", () => {
       id: 3,
       title: "Ванна",
       tab: "CleaningBath",
-      image: "src/assets/images/footer/bathroom.png",
+      image: bathImg,
     },
     {
       id: 4,
